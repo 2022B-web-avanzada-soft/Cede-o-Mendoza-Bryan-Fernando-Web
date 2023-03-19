a67492ef194d93f1a366b0ff3b1c53c42835e225
Fernando Cedeño <bryan.cedeno01@epn.edu.ec>
2023-03-19 12:10:50 -0500

    Dto

--- 04-nestjs-websockets/websockets/src/usuario/usuario.service.ts
+++ 04-nestjs-websockets/websockets/src/usuario/usuario.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from "@nestjs/common";
 import {InjectDataSource} from "@nestjs/typeorm";
-import {DataSource} from "typeorm";
+import {DataSource, FindManyOptions} from "typeorm";
+import {UsuarioEntity} from "./usuario.entity";
+import {UsuarioCreateDto} from "../dto/usuario-create.dto";
+import {UsuarioUpdateDto} from "../dto/usuario-update.dto";
 
 @Injectable()
 export class UsuarioService{
@@ -8,5 +11,28 @@ export class UsuarioService{
         @InjectDataSource()
         public datasource: DataSource
     ) {}
+    public usuarioRepository = this.datasource.getRepository(UsuarioEntity);
+    find(opciones: FindManyOptions<UsuarioEntity>) {
+        return this.usuarioRepository.find(opciones)
+    }
+    findOneById(id: number) {
+        return this.usuarioRepository.findOne({
+            //select:{ }, el ide me ayuda a ver que atributos tiene esta entidad y cuales selecionaar.
+            where: {
+                id: id
+            },
+        })
+    }
+    create(datosCrear: UsuarioCreateDto) {
+        return this.usuarioRepository.save(datosCrear);
+    }
+    update(datosActualizar: UsuarioUpdateDto, id: number) {
+        return this.usuarioRepository.save(
+            {...datosActualizar, id}
+        );
+    }
+    delete(id: number) {
+        return this.usuarioRepository.delete(id);
+    }
 
 }
